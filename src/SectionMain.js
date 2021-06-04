b416f0544d95b41602a82bacd1aeba0fb610f3f7
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-03 22:55:25 -0400

    Update SectionMain.js

--- src/SectionMain.js
+++ src/SectionMain.js
@@ -1,14 +1,22 @@
+// imports react components from react
 import React, { Component } from 'react';
+// imports sectionMain styling
 import './SectionMain.css';
 
+// creates new component sectionMain
 class SectionMain extends Component {
+  // will render the following
   render() {
+    // will return the following html
     return (
+        // a section with class name sectionMain
         <section className="SectionMain">
         </section>
 
     );
   }
 }
+
+// exports single component sectionMain
 
 export default SectionMain;
