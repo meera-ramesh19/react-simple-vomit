f82b3638ce18aebb686dbfe82b7b192e0c3f4f1f
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-03 22:54:45 -0400

    commenting for understanding

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,22 @@
+// imports components from react
 import React, { Component } from 'react';
+// imports header styling
 import './Header.css';
 
+// creates new component header
 class Header extends Component {
+  // renders the following
   render() {
+    // creates style variable to be passed to header
     const style = {
+      // sets width to whatever is set in app.js
       width: this.props.width,
+      // sets background color to whatever is set in app.js
       backgroundColor: this.props.backColor
     }
+    // will return the following html
     return (
+        // header, with styles as defined above and classname header-main
         <header style={style} className="Header-main">
         </header>
 
@@ -15,6 +24,7 @@ class Header extends Component {
   }
 }
 
+// exports single component header
 export default Header;
 
 
