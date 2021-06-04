047b726d2addd2b4c8735f9432e7fee0c8c5f388
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-03 22:53:48 -0400

    commenting for understanding

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,22 @@
+// import components from react
 import React, { Component } from 'react';
+// import footer styling
 import './Footer.css';
 
+// create new component footer
 class Footer extends Component {
+  // renders the following
   render() {
+    // returns the following html 
     return (
+        // footer, with added prop className footer
         <footer className="Footer">
         </footer>
 
     );
   }
 }
+
+// exports single footer component
 
 export default Footer;
