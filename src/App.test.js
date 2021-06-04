8e79bb2198194254aa84080ec4f4e57f46ad9acf
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-03 22:51:31 -0400

    commetnting for understanding

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
+  
+// imports react components
 import React from 'react';
+// import react components for interacting with DOM
 import ReactDOM from 'react-dom';
+// import App class that is exported from App file
 import App from './App';
 
+// tests if App renders without crashing in a real browser environment
 it('renders without crashing', () => {
+  // creating a div to test with
   const div = document.createElement('div');
+  // runs a test rendering with App and div
   ReactDOM.render(<App />, div);
 });
