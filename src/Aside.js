107e243dd09cb03e04981403f182e11b8a3679ac
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-03 22:52:52 -0400

    commenting for understanding

--- src/Aside.js
+++ src/Aside.js
@@ -1,14 +1,22 @@
+// import react components from react
 import React, { Component } from 'react';
+// import aside styling
 import './Aside.css';
 
+// creates new component Aside
 class Aside extends Component {
+  // renders the component
   render() {
+    // it will return the following element
     return (
+        // will return Aside, adds prop className Aside
         <aside className="Aside">
         </aside>
 
     );
   }
 }
+
+// exports single Aside component
 
 export default Aside;
